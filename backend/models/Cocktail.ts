0cfe7c1b7679c05ef790e86eecafa9909fd7b0e0
Kwon Valerii <wellko24@gmail.com>
2023-03-28 23:10:33 +0600

    added Cocktail model

---
+++ backend/models/Cocktail.ts
@@ -0,0 +1,41 @@
+import mongoose, { Types } from "mongoose";
+import User from "./User";
+import {Cocktail} from "../types";
+
+const Schema = mongoose.Schema;
+const CocktailSchema = new Schema<Cocktail>({
+	name: {
+		type: String,
+		required: true,
+	},
+	image: {
+		type: String,
+		required: true,
+	},
+	receipt: {
+		type: String,
+		required: true,
+	},
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+		validate: {
+			validator: async (value: Types.ObjectId) => User.findById(value),
+			message: "User not found!",
+		},
+	},
+	isPublished: {
+		type: Boolean,
+		required: true,
+		default: false,
+	},
+	ingredients: {
+		type: [],
+		required: true,
+	}
+});
+
+const Cocktail = mongoose.model("Cocktail", CocktailSchema);
+
+export default Cocktail;

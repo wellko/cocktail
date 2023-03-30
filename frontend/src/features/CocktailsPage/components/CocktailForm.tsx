630d4ca34fe2890961bc927b952f9db03fb79764
Kwon Valerii <wellko24@gmail.com>
2023-03-30 20:37:27 +0600

    added delete button for input in Form

--- frontend/src/features/CocktailsPage/components/CocktailForm.tsx
+++ frontend/src/features/CocktailsPage/components/CocktailForm.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import { Box, Button , Grid, TextField} from '@mui/material';
+import React, {useState} from 'react';
+import {Box, Button, Grid, IconButton, TextField} from '@mui/material';
 import FileInput from '../../../components/UI/FileInput/FileInput';
-import { useAppDispatch, useAppSelector } from '../../../app/hooks';
-import { useNavigate } from 'react-router-dom';
-import { LoadingButton } from '@mui/lab';
+import {useAppDispatch, useAppSelector} from '../../../app/hooks';
+import {useNavigate} from 'react-router-dom';
+import {LoadingButton} from '@mui/lab';
 import {selectStatusOfPostingCocktails} from "../CocktailsPageSlice";
 import {CocktailMutation} from "../../../types";
 import {createCocktails} from "../CocktailsPageThunks";
+import CloseIcon from "@mui/icons-material/Close";
 
 const CocktailForm = () => {
 	const dispatch = useAppDispatch();
@@ -39,23 +40,31 @@ const CocktailForm = () => {
 	};
 
 	const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
+		const {name, value} = e.target;
 		setState((prevState) => {
-			return { ...prevState, [name]: value };
+			return {...prevState, [name]: value};
 		});
 	};
 
 	const inputIngredientChangeHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, index: number) => {
-		const { name, value } = e.target;
+		const {name, value} = e.target;
 		setState((prevState) => {
 			const updated = [...prevState.ingredients];
-			updated[index] = { ...updated[index], [name]: value };
-			return { ...prevState, ingredients: updated };
+			updated[index] = {...updated[index], [name]: value};
+			return {...prevState, ingredients: updated};
 		});
 	};
 
+	const deleteInput = (index: number) => {
+		setState((prevState) => {
+			const updated = [...prevState.ingredients];
+			updated.splice(index, 1);
+			return {...prevState, ingredients: updated};
+		})
+	};
+
 	const fileInputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, files } = e.target;
+		const {name, files} = e.target;
 		setState((prevState) => ({
 			...prevState,
 			[name]: files && files[0] ? files[0] : null,
@@ -65,8 +74,8 @@ const CocktailForm = () => {
 	const addNewField = () => {
 		setState((prevState) => {
 			const updated = [...prevState.ingredients];
-			updated.push({ name: '', amount: '' });
-			return { ...prevState, ingredients: updated };
+			updated.push({name: '', amount: ''});
+			return {...prevState, ingredients: updated};
 		})
 	}
 
@@ -75,7 +84,7 @@ const CocktailForm = () => {
 			<Grid container direction="row" spacing={2}>
 				<Grid container item xs={12}>
 					<TextField
-						sx={{ margin: 'auto', width: '60%' }}
+						sx={{margin: 'auto', width: '60%'}}
 						label="Cocktail"
 						name="name"
 						value={state.name}
@@ -85,30 +94,39 @@ const CocktailForm = () => {
 				</Grid>
 
 				<Grid container item xs={12}>
-					{state.ingredients.map((el, index) =><Box width='60%' marginX='auto' marginBottom={2}>
-						<TextField
-							sx={{ width: '80%' }}
-							label="ingredient"
-							name="name"
-							value={state.ingredients[index].name}
-							onChange={(e) => inputIngredientChangeHandler(e, index)}
-							required
-						/>
-						<TextField
-							sx={{ width: '15%', marginLeft: '5%'}}
-							label="amount"
-							name="amount"
-							value={state.ingredients[index].amount}
-							onChange={(e) => inputIngredientChangeHandler(e, index)}
-							required
-						/>
-					</Box>
+					{state.ingredients.map((el, index) => <Box width='60%' marginX='auto' marginBottom={2}>
+							<TextField
+								sx={{width: '75%'}}
+								label="ingredient"
+								name="name"
+								value={state.ingredients[index].name}
+								onChange={(e) => inputIngredientChangeHandler(e, index)}
+								required
+							/>
+							<TextField
+								sx={{width: '15%', marginLeft: '5%'}}
+								label="amount"
+								name="amount"
+								value={state.ingredients[index].amount}
+								onChange={(e) => inputIngredientChangeHandler(e, index)}
+								required
+							/>
+						{index !== 0 && <IconButton
+							aria-label="close"
+							color="inherit"
+							size="small"
+							onClick={() => deleteInput(index)}
+						>
+							<CloseIcon fontSize='large'/>
+						</IconButton>}
+						</Box>
 					)}
 				</Grid>
-				<Button variant='outlined' sx={{margin: 'auto'}} type='button' onClick={addNewField}> add new ingredient</Button>
+				<Button variant='outlined' sx={{margin: 'auto'}} type='button' onClick={addNewField}> add new
+					ingredient</Button>
 				<Grid container item xs={12}>
 					<TextField
-						sx={{ margin: 'auto', width: '60%' }}
+						sx={{margin: 'auto', width: '60%'}}
 						multiline
 						rows={3}
 						id="info"
@@ -121,12 +139,13 @@ const CocktailForm = () => {
 
 				<Grid container item xs={12} justifyContent="center">
 					<Grid item xs={6}>
-						<FileInput label="Image" onChange={fileInputChangeHandler} name="image" type="image/*" />
+						<FileInput label="Image" onChange={fileInputChangeHandler} name="image" type="image/*"/>
 					</Grid>
 				</Grid>
 
 				<Grid container item xs={12}>
-					<LoadingButton sx={{ margin: 'auto' }} loading={loading} type="submit" color="primary" variant="contained">
+					<LoadingButton sx={{margin: 'auto'}} loading={loading} type="submit" color="primary"
+								   variant="contained">
 						Create
 					</LoadingButton>
 				</Grid>
